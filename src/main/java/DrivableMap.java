525cc1ca99fcbd040651ca31d635a96c23f170b0
arvin <arvin.gingoyon@outlook.com>
2022-10-08 11:33:26 -0400

    Completed all TODO's note: removed Abstract from Trader so it's not an abstract class anymore, not sure if we we're allowed to do this but i think tests would have run if this wasnt done

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -7,6 +7,7 @@
  * created the constructor for you already.
  */
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,8 +28,13 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
-
-
+    public boolean addDrivable(String id, Drivable item) {
+        if (!drivable_map.containsKey(id)) {
+            drivable_map.put(id, item);
+            return true;
+        }
+        return false;
+    }
 
 
     /* TODO: Write a method named hasFasterThan that takes an int (a speed)
@@ -37,8 +43,15 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
+    public boolean hasFasterThan(int speed) {
+        for(Drivable item : drivable_map.values()) {
+            if(item.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
 
-
+        return false;
+    }
 
 
 
@@ -46,8 +59,15 @@ class DrivableMap {
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
+    public List<Tradable> getTradable() {
+        ArrayList<Tradable> tradables = new ArrayList<Tradable>();
+        for(Drivable item : drivable_map.values()) {
+            if(item instanceof Tradable) {
+                tradables.add((Tradable) item);
+            }
+        }
 
+        return tradables;
+    }
 
-
-    
 }
